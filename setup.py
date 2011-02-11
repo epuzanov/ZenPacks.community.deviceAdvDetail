ddf37f8857cbc3dd15541c1fbb48009d27985aba
Egor Puzanov <epuzanov@gmx.de>
2011-02-12 00:12:41 +0100

    fix empty memory slots details error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.deviceAdvDetail"
-VERSION = "2.7.2"
+VERSION = "2.7.3"
 AUTHOR = "Egor Puzanov"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
