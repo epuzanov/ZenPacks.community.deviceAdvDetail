d18aa923588870bc587cb4916d0c9df9fb1aadc2
Egor Puzanov <epuzanov@gmx.de>
2012-10-31 19:25:21 +0100

    transform fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.deviceAdvDetail"
-VERSION = "2.8.70"
+VERSION = "2.8.71"
 AUTHOR = "Egor Puzanov"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
