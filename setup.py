9c534484555ce70c9b54ec0476f50d454810501f
Egor Puzanov <epuzanov@gmx.de>
2012-11-19 20:16:43 +0100

    added LICENSE.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = "ZenPacks.community.deviceAdvDetail"
 VERSION = "2.9.0"
 AUTHOR = "Egor Puzanov"
-LICENSE = ""
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.deviceAdvDetail']
 INSTALL_REQUIRES = []
